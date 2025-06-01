af922fd25d5763ba61d8822b4f1bf4206e621f6f
HeetJani123 <164028420+HeetJani123@users.noreply.github.com>
2025-06-01 22:29:12 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'project'
+  plugins: [react()],
+  root: 'project',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
 }); 
